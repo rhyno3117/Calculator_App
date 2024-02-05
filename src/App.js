233960509093b4a996d3c202bc03e72be810a9ae
ryan <rhynogolfer42@gmail.com>
2024-02-05 11:57:06 -0800

    Adds myfile.md

--- src/App.js
+++ src/App.js
@@ -1,34 +1,37 @@
 import './App.css';
 
 import Button from './components/Button';
+import Input from './components/Input';
+
 
 const App = () => {
   return (
     <div className="App">
       <div className='calc-wrapper'>
+        <Input />
         <div className="row">
-        <Button symbol="7"/>
-        <Button symbol="8"/>
-        <Button symbol="9"/>
-        <Button symbol="/"/>
+          <Button symbol="7" />
+          <Button symbol="8" />
+          <Button symbol="9" />
+          <Button symbol="/" />
         </div>
         <div className="row">
-        <Button symbol="4"/>
-        <Button symbol="5"/>
-        <Button symbol="6"/>
-        <Button symbol="*"/>
+          <Button symbol="4" />
+          <Button symbol="5" />
+          <Button symbol="6" />
+          <Button symbol="*" />
         </div>
         <div className="row">
-        <Button symbol="1"/>
-        <Button symbol="2"/>
-        <Button symbol="3"/>
-        <Button symbol="+"/>
+          <Button symbol="1" />
+          <Button symbol="2" />
+          <Button symbol="3" />
+          <Button symbol="+" />
         </div>
         <div className="row">
-        <Button symbol="0"/>
-        <Button symbol="."/>
-        <Button symbol="="/>
-        <Button symbol="-"/>
+          <Button symbol="0" />
+          <Button symbol="." />
+          <Button symbol="=" />
+          <Button symbol="-" />
 
         </div>
       </div>
---
+++ src/components/Input.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import './Input.css'
+
+function Input() {
+    return (
+        <div className='input-wrapper'>
+            <div className='result'>
+                <h1>Result</h1>
+            </div>
+            <div className='text'>
+                <h3>Text</h3>
+            </div>
+        </div>
+    )
+}
+
+export default Input
