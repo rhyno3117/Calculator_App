7baa2682cac13e0dbe95ae7adfd3ad7004e6c0e6
ryan <rhynogolfer42@gmail.com>
2024-02-04 22:24:48 -0800

    Layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import './App.css';
 
+import Button from './components/Button';
+
 const App = () => {
   return (
-    <div>
+    <div className="App">
       <div className='calc-wrapper'>
-<h1>Heading</h1>
+        <div className="row">
+        <Button symbol="7"/>
+        <Button symbol="8"/>
+        <Button symbol="9"/>
+        <Button symbol="/"/>
+        </div>
+        <div className="row">
+        <Button symbol="4"/>
+        <Button symbol="5"/>
+        <Button symbol="6"/>
+        <Button symbol="*"/>
+        </div>
+        <div className="row">
+        <Button symbol="1"/>
+        <Button symbol="2"/>
+        <Button symbol="3"/>
+        <Button symbol="+"/>
+        </div>
+        <div className="row">
+        <Button symbol="0"/>
+        <Button symbol="."/>
+        <Button symbol="="/>
+        <Button symbol="-"/>
+
+        </div>
       </div>
     </div>
   );
---
+++ src/components/Button.js
@@ -0,0 +1,9 @@
+import './Button.css';
+
+const Button = ({symbol}) => {
+    return (
+        <div className="button-wrapper">{symbol}</div>
+    )
+}
+
+export default Button;
